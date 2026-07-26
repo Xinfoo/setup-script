40651437a3d4045c711fa9f577f1c2fc3fc4f8a2
Xinfoo <13994062125@163.com>
2026-07-27 00:22:55 +0800

    增加setup.sh脚本。

--- live/setup.sh
+++ live/setup.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
-source "./functions.sh"
-source "./use-local-mirror.sh"
+
+# 初始化
+SRC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SRC_DIR/functions.sh"
+source "$SRC_DIR/use-local-mirror.sh"
 
 # 权限检查
 permission_check
@@ -9,4 +12,10 @@ permission_check
 # 是否使用移动硬盘镜像站
 if confirm "Do you want to use local mirror?"; than
     use_local_miror
+else
+    # 不使用本地镜像站则检查网络
+    if ! ping -c 3 baidu.com >/dev/null 2>&1; then
+        echo 'Need Network Connection...'
+        exit 1
+    fi
 fi
