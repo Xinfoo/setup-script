6053ce0b124e787dec2f50fdcd58e161b3d81497
Xinfoo <13994062125@163.com>
2026-07-26 22:52:31 +0800

    分离部分函数。

--- live/partition-and-format.sh
+++ live/partition-and-format.sh
@@ -8,33 +8,32 @@ permission_check() {
     fi
 }
 
-disk_selector() {
+disk_detector() {
     # 定义要使用的变量和数组
     local disk
-    local PS3="Select a disk: "
-    local -a disk_list=()
+    local disk_list=()
 
     # 循环检测块设备，将可用设备加入数组
-    for block_path in "/sys/block/"*; do
+    for disk_sys_dir in /sys/block/*; do
         # 检查块设备目录
-        if [[ ! -d "$block_path" ]]; then
+        if [[ ! -d "$disk_sys_dir" ]]; then
             continue
         fi
 
         # 检查是否是虚拟块设备
-        if [[ ! -e "$block_path/device" ]]; then
+        if [[ ! -e "$disk_sys_dir/device" ]]; then
             continue
         fi
 
         # 检查是否是USB块设备
-        if [[ "$(lsblk --noheadings --nodeps --raw --output TRAN "/dev/$(basename "$block_path")")" == "usb" ]]; then
+        if [[ "$(lsblk --noheadings --nodeps --raw --output TRAN "/dev/$(basename "$disk_sys_dir")")" == "usb" ]]; then
             continue
         fi
 
         # 检查是否是可移动设备
-        if [[ -f "$block_path/removable" ]] && [[ "$(cat "$block_path/removable")" == "0" ]];then
+        if [[ -f "$disk_sys_dir/removable" ]] && [[ "$(cat "$disk_sys_dir/removable")" == "0" ]];then
             # 将扫描到的块设备全路径加入数组列表
-            disk="/dev/$(basename "$block_path")"
+            disk="$(basename "$disk_sys_dir")"
             disk_list+=("$disk")
         fi
     done
@@ -45,12 +44,28 @@ disk_selector() {
         return 1
     fi
 
+    # 输出数组为列表
+    echo "${disk_list[@]}"
+}
+
+disk_selector() {
+    # 定义要使用的变量
+    local disk_dev_path
+    local PS3="Select a disk: "
+    local -a disk_dev_path_list=()
+
+    # 循环处理列表为数组并添加全路径
+    for disk in $(disk_detector); do
+        disk_dev_path="/dev/$disk"
+        disk_dev_path_list+=("$disk_dev_path")
+    done
+
     # 列出块设备信息
-    lsblk -f -o NAME,FSTYPE,SIZE,MOUNTPOINT,FSUSED,FSAVAIL,FSUSE% "${disk_list[@]}" >&2
+    lsblk -f -o NAME,FSTYPE,SIZE,MOUNTPOINT,FSUSED,FSAVAIL,FSUSE% "${disk_dev_path_list[@]}" >&2
     echo >&2
 
     # 选择块设备并把块设备全路径输出到标准输出
-    select choice in "${disk_list[@]}"; do
+    select choice in ${disk_dev_path_list[@]}; do
         if [[ -n $choice ]]; then
             echo "$choice"
             return 0
@@ -64,70 +79,42 @@ disk_selector() {
 
 partition_selector() {
     # 定义要使用的变量和数组
-    local block
-    local partition
+    local disk_dev_path
+    local partition_dev_path
     local PS3="Select a partition: "
-    local -a block_list=()
-    local -a dev_block_list=()
-    local -a partition_list=()
-
-    # 循环检测块设备，将可用设备加入数组
-    for block_path in "/sys/block/"*; do
-        # 检查块设备目录
-        if [[ ! -d "$block_path" ]]; then
-            continue
-        fi
-
-        # 检查是否是虚拟块设备
-        if [[ ! -e "$block_path/device" ]]; then
-            continue
-        fi
-
-        # 检查是否是USB块设备
-        if [[ "$(lsblk --noheadings --nodeps --raw --output TRAN "/dev/$(basename "$block_path")")" == "usb" ]]; then
-            continue
-        fi
-
-        # 检查是否是可移动设备
-        if [[ -f "$block_path/removable" ]] && [[ "$(cat "$block_path/removable")" == "0" ]];then
-            # 将扫描到的块设备加入数组列表
-            block="/dev/$(basename "$block_path")"
-            block_list+=("$block_path")
-            dev_block_list+=("$block")
-        fi
-    done
-
-    # 检查数组是否为空
-    if [[ "${#block_list[@]}" == "0" ]];then
-        echo "No usable disks available!" >&2
-        return 1
-    fi
+    local -a disk_dev_path_list=()
+    local -a partition_dev_path_list=()
 
     # 循环处理分区列表数组
-    for block_path in "${block_list[@]}"; do
+    for disk in $(disk_detector); do
         # 检查磁盘是否有分区
-        if [[ -z "$(ls "$block_path" | grep "$(basename "$block_path")")" ]]; then
+        if [[ -z "$(ls "/sys/block/$disk" | grep "$disk")" ]]; then
             continue
         fi
-        # 将分区加入数组
-        for partition in "$block_path/"$(basename "$block_path")*; do
-            partition="/dev/$(basename "$partition")"
-            partition_list+=("$partition")
+
+        # 循环处理磁盘列表为数组并添加全路径
+        disk_dev_path="/dev/$disk"
+        disk_dev_path_list+=("$disk_dev_path")
+
+        # 循环将磁盘下的分区加入数组
+        for partition_sys_dir in /sys/block/$disk/$disk*; do
+            partition_dev_path="/dev/$(basename "$partition_sys_dir")"
+            partition_dev_path_list+=("$partition_dev_path")
         done
     done
 
     # 检查数组是否为空
-    if [[ "${#partition_list[@]}" == "0" ]];then
+    if [[ "${#partition_dev_path_list[@]}" == "0" ]];then
         echo "No usable partitions available!" >&2
         return 1
     fi
 
     # 列出块设备信息
-    lsblk -f -o NAME,FSTYPE,SIZE,MOUNTPOINT,FSUSED,FSAVAIL,FSUSE% "${dev_block_list[@]}" >&2
+    lsblk -f -o NAME,FSTYPE,SIZE,MOUNTPOINT,FSUSED,FSAVAIL,FSUSE% "${disk_dev_path_list[@]}" >&2
     echo >&2
 
     # 选择分区并把分区全路径输出到标准输出
-    select choice in "${partition_list[@]}"; do
+    select choice in ${partition_dev_path_list[@]}; do
         if [[ -n $choice ]]; then
             echo "$choice"
             return 0
@@ -148,7 +135,7 @@ disk_wiper() {
     # 清除分区表
     wipefs -a "$1"
 
-    # 如果目标为SSD，则整盘清零
+    # 如果目标为支持丢弃的SSD，则整盘清零
     if [[ "$(lsblk --noheadings --nodeps --raw --output ROTA "$1")" == "0" ]] && [[ "$(lsblk --discard --noheadings --nodeps --raw --output DISC-GRAN "$1")" != "0B" ]]; then
         blkdiscard -f "$1"
     fi
@@ -156,3 +143,4 @@ disk_wiper() {
 
 disk_selector
 partition_selector
+disk_detector
