d2707a345c20c8b92c6ba2da117e8bff71200d98
Xinfoo <13994062125@163.com>
2026-07-26 20:56:22 +0800

    新增分区选择器，补充更多注释。

--- live/partition-and-format.sh
+++ live/partition-and-format.sh
@@ -58,17 +58,85 @@ disk_selector() {
             echo "Invalid selection, please choose a number from the list." >&2
         fi
     done
+
+    return 1
+}
+
+partition_selector() {
+    local partition
+    local PS3="Select a partition: "
+    local -a block_list=()
+    local -a partition_list=()
+
+    # 循环检测块设备，将可用设备加入数组
+    for block_path in "/sys/block/"*; do
+        # 检查块设备目录
+        if [[ ! -d "$block_path" ]]; then
+            continue
+        fi
+
+        # 检查是否是虚拟块设备
+        if [[ ! -e "$block_path/device" ]]; then
+            continue
+        fi
+
+        # 检查是否是USB块设备
+        if [[ "$(lsblk --noheadings --nodeps --raw --output TRAN "/dev/$(basename "$block_path")")" == "usb" ]]; then
+            continue
+        fi
+
+        # 检查是否是可移动设备
+        if [[ -f "$block_path/removable" ]] && [[ "$(cat "$block_path/removable")" == "0" ]];then
+            # 将扫描到的块设备加入数组列表
+            block_list+=("$block_path")
+        fi
+    done
+
+    # 检查数组是否为空
+    if [[ "${#block_list[@]}" == "0" ]];then
+        echo "No usable disks available!" >&2
+        return 1
+    fi
+
+    # 循环处理分区列表数组
+    for block_path in "${block_list[@]}"; do
+        for partition in "$block_path/"$(basename "$block_path")*; do
+            partition="/dev/$(basename "$partition")"
+            partition_list+=("$partition")
+        done
+    done
+
+    # 列出块设备信息
+    lsblk -f -o NAME,FSTYPE,SIZE,MOUNTPOINT,FSUSED,FSAVAIL,FSUSE% "${disk_list[@]}" >&2
+    echo >&2
+
+    # 选择分区并把分区全路径输出到标准输出
+    select choice in "${partition_list[@]}"; do
+        if [[ -n $choice ]]; then
+            echo "$choice"
+            return 0
+        else
+            echo "Invalid selection, please choose a number from the list." >&2
+        fi
+    done
+
     return 1
 }
 
 disk_wiper() {
+    # 检查输入是否为空
     if [[ -z "$1" ]]; then
         return 1
     fi
+
+    # 清除分区表
     wipefs -a "$1"
+
+    # 如果目标为SSD，则整盘清零
     if [[ "$(lsblk --noheadings --nodeps --raw --output ROTA "$1")" == "0" ]] && [[ "$(lsblk --discard --noheadings --nodeps --raw --output DISC-GRAN "$1")" != "0B" ]]; then
         blkdiscard -f "$1"
     fi
 }
 
 disk_selector
+partition_selector
