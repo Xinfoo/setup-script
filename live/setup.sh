f26cdd24fd70788d94a4ff6b9bfb61603f86533b
Xinfoo <13994062125@163.com>
2026-07-27 01:13:13 +0800

    硬盘擦除器工作完成。

--- live/setup.sh
+++ live/setup.sh
@@ -5,6 +5,11 @@ set -euo pipefail
 SRC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SRC_DIR/functions.sh"
 source "$SRC_DIR/use-local-mirror.sh"
+# source "./functions.sh"
+# source "./use-local-mirror.sh"
+
+SELECTED_DISK=""
+TEMP_CONF_ITEM=""
 
 # 权限检查
 permission_check
@@ -19,3 +24,25 @@ else
         exit 1
     fi
 fi
+
+# 是否擦除磁盘全新安装
+while true; do
+    if confirm "Do you want to erase some disks?"; then
+        TEMP_CONF_ITEM="1"
+        break
+    else
+        break
+    fi
+done
+while true; do
+    if [[ "$TEMP_CONF_ITEM" == "1" ]]; then
+        SELECTED_DISK="$(disk_selector)"
+        if confirm "Are you sure you want to erase $SELECTED_DISK ? (Data connot be recovered)"; then
+            disk_wiper "$SELECTED_DISK"
+        fi
+        if confirm "Do you want to erase the other disks?"; then
+            continue
+        fi
+        break
+    fi
+done
