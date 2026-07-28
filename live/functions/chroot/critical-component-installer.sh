e2afb1e85f4e6d171e14fb80fb5e5df6c0b61e01
Xinfoo <13994062125@163.com>
2026-07-28 23:53:30 +0800

    编写了重要组件安装器。

--- live/functions/chroot/critical-component-installer.sh
+++ live/functions/chroot/critical-component-installer.sh
@@ -1,4 +1,28 @@
 #!/usr/bin/env bash
 
 # 重要组件安装器
-critical_component_installer() {}
+critical_component_installer() {
+    # zsh相关软件包列表
+    local -a zsh_packages_list=(
+        "zsh"
+        "zsh-completions"
+        "zsh-autosuggestions"
+        "zsh-syntax-highlighting"
+        "grml-zsh-config"
+        )
+
+    # 网络相关软件包列表
+    local -a network_packages_list=(
+        "networkmanager"
+        "iwd"
+        "dhcpcd"
+        "iftop"
+        "nethogs"
+        )
+
+    # 安装zsh
+    pacman -S --needed --noconfirm ${zsh_packages_list[@]}
+
+    # 安装网络
+    pacman -S --needed --noconfirm ${network_packages_list[@]}
+}
