eeea7de3e76e9a55e1bd75f79c5a628f6c91e312
Xinfoo <13994062125@163.com>
2026-07-28 13:27:20 +0800

    修改格式化器。

--- live/functions/actuator/manual-partitioner.sh
+++ live/functions/actuator/manual-partitioner.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+
+# 手动分区器 
--- live/functions/actuator/partition-formatter.sh
+++ live/functions/actuator/partition-formatter.sh
@@ -2,29 +2,31 @@
 
 # 分区格式化器
 partition_formatter() {
-    if [[ -z "${1:-}" ]]; then
-        return 1
-    fi
+    local file_system
+    local partition_dev_path
 
-    # 从关联数组中获取所要格式化的文件系统
-    local file_system="${file_system_choices["$1"]}"
+    for partition_dev_path in "${!file_system_choices[@]}"; do
 
-    # 格式化文件系统
-    case "$file_system" in
-        "vfat")
-            mkfs.fat -F 32 "$1"
-            ;;
-        "ext4")
-            mkfs.ext4 -F "$1"
-            ;;
-        "xfs")
-            mkfs.xfs -f "$1"
-            ;;
-        "f2fs")
-            mkfs.f2fs -f -O extra_attr,inode_checksum,sb_checksum,compression "$1"
-            ;;
-        "swap")
-            mkswap -f "$1"
-            ;;
-    esac
+        # 从关联数组中获取所要格式化的文件系统
+        file_system="${file_system_choices["$partition_dev_path"]}"
+
+        # 格式化文件系统
+        case "$file_system" in
+            "vfat")
+                mkfs.fat -F 32 "$file_system_dev_path"
+                ;;
+            "ext4")
+                mkfs.ext4 -F "$file_system_dev_path"
+                ;;
+            "xfs")
+                mkfs.xfs -f "$file_system_dev_path"
+                ;;
+            "f2fs")
+                mkfs.f2fs -f -O extra_attr,inode_checksum,sb_checksum,compression "$file_system_dev_path"
+                ;;
+            "swap")
+                mkswap -f "$file_system_dev_path"
+                ;;
+        esac
+    done
 }
