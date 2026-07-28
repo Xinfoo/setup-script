e870214b7859159d7b37b81aaeed86da9148f460
Xinfoo <13994062125@163.com>
2026-07-28 11:30:38 +0800

    完善自动分区器。

--- live/functions/automatic-partitioner.sh
+++ live/functions/automatic-partitioner.sh
@@ -10,26 +10,49 @@ automatic_partitioner() {
     local choice
     local swap_size_mib
     local PS3="Select a partitioning plan: "
+
+    local -a options=(
+        "| EFI (1GiB) | ROOT (AUTO) | SWAP (AUTO) |"
+        "| EFI (1GiB) | ROOT (100GiB) | HOME (AUTO) | SWAP (AUTO) |"
+        "| EFI (1GiB) | ROOT (AUTO) |"
+        )
+
     swap_size_mib="$(get_swap_size)" || return 1
 
-    echo "1>| EFI (1GiB) | ROOT (AUTO) | SWAP (AUTO) |" >&2
-    echo "2>| EFI (1GiB) | ROOT (100GiB) | HOME (AUTO) | SWAP (AUTO) |" >&2
-    echo "3>| EFI (1GiB) | ROOT (AUTO) |" >&2
-    select choice in plan1 plan2 plan3; do
-        case $choice in
-            plan1)
+    select choice in "${options[@]}"; do
+        case "$REPLY" in
+            1)
                 sfdisk "$1" <<EOF
 label: gpt
 unit: MiB
 
 size=1024,type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
 size=-$swap_size_mib,type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
-type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
+size=$swap_size_mib,type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
 EOF
+                return 0
                 ;;
-            plan2)
+            2)
+                sfdisk "$1" <<EOF
+label: gpt
+unit: MiB
+
+size=1024,type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+size=102400,type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
+size=-$swap_size_mib,type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
+size=$swap_size_mib,type=0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
+EOF
+                return 0
                 ;;
-            plan3)
+            3)
+                sfdisk "$1" <<EOF
+lobel: gpt
+unit: MiB
+
+size=1024,type=C12A7328-F81F-11D2-BA4B-00A0C93EC93B
+size=,type=0FC63DAF-8483-4772-8E79-3D69D8477DE4
+EOF
+                return 0
                 ;;
             *)
                 echo "Please enter a valid number." >&2
