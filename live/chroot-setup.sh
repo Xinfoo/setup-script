7bf9b31a201d96260ad1807407b5e05640d13203
Xinfoo <13994062125@163.com>
2026-07-29 11:02:17 +0800

    完成setup.sh

--- live/chroot-setup.sh
+++ live/chroot-setup.sh
@@ -18,3 +18,30 @@ DESKTOP_ENVIRONMENT=""
 INPUT_METHOD=""
 FIREWALL=""
 PRINTER=""
+
+# 权限检查
+permission_check
+
+# 基础系统配置
+basic_setter
+
+# 安装重要组件
+critical_component_installer
+
+# 安装额外驱动
+extra_driver_installer
+
+# 安装桌面环境
+desktop_environment_installer
+
+# 安装额外软件
+extra_software_installer
+
+# 安装引导
+bootloader_installer
+
+# 最终配置
+final_setter
+
+echo "System configuration complete."
+echo "Please enter "exit" to manually exit the chroot environment."
