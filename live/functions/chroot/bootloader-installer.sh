a0a7acd348699a6e6fbf3d402eaba054b6e006b9
Xinfoo <13994062125@163.com>
2026-07-29 10:57:29 +0800

    继续修复引导管理器的问题。

--- live/functions/chroot/bootloader-installer.sh
+++ live/functions/chroot/bootloader-installer.sh
@@ -6,19 +6,32 @@ bootloader_installer() {
     local micro_code="$(cat "$SRC_DIR/info/micro-code-img.txt")"
     local initramfs="$(cat "$SRC_DIR/info/initramfs-img.txt")"
     local root_uuid="$(cat "$SRC_DIR/info/root-uuid.txt")"
+
     bootctl install
 
-    echo "title Arch Linux
+    if [[ -z "$micro_code" ]]; then
+        echo "title Arch Linux
+linux /$kernel
+initrd /$initramfs
+options root=UUID=$root_uuid rw loglevel=3" > "/boot/loader/entries/arch.conf"
+
+        echo "title Arch Linux Fallback
+linux /$kernel
+initrd /$initramfs
+options root=UUID=$root_uuid rw loglevel=3" > "/boot/loader/entries/arch-fallback.conf"
+    else
+        echo "title Arch Linux
 linux /$kernel
 initrd /$micro_code
 initrd /$initramfs
 options root=UUID=$root_uuid rw loglevel=3" > "/boot/loader/entries/arch.conf"
 
-    echo "title Arch Linux Fallback
+        echo "title Arch Linux Fallback
 linux /$kernel
 initrd /$micro_code
 initrd /$initramfs
 options root=UUID=$root_uuid rw loglevel=3" > "/boot/loader/entries/arch-fallback.conf"
+    fi
 
     echo "default arch.conf
 editor no
